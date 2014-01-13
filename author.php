5b6e3761df73146a7688bc4048d9a2f6e47d2b99
Scott Donaldson <scott.p.donaldson@gmail.com>
2014-01-13 12:30:50 -0500

    cleaned up author pages and header.php, adding some static assets

---
+++ author.php
@@ -0,0 +1,22 @@
+<?php get_header(); 
+$curauth = isset($_GET['author_name']) ? get_user_by('slug', $author_name) : get_userdata(intval($author));
+$post = $posts[0];
+?>
+
+<div class="content-title">
+	<p>All posts by <?= $curauth->display_name; ?>
+		<?php if ( get_field('google_profile', 'user_' . $curauth->ID )) { ?>
+	        <a rel="me" href="https://profiles.google.com/scott.p.donaldson">
+			 	<img src="http://www.google.com/images/icons/ui/gprofile_button-16.png" width="16" height="16">
+			</a>
+		<?php } ?>
+	</p>
+	<?php the_field('bio', 'user_' . $curauth->ID); ?>
+
+</div>
+
+<?php
+get_template_part('loop');
+get_template_part('pagination');
+
+get_footer(); ?>
--- header.php
+++ header.php
@@ -1,14 +1,15 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
     <head>
-        <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+        <meta charset="UTF-8">
         <meta property="fb:admins" content="1543242988,8643044" />
         <title><?php wp_title(''); ?></title>
-        <meta http-equiv="Content-language" content="<?php bloginfo('language'); ?>" />
-		<link rel="profile" href="http://gmpg.org/xfn/11" />
-        <link rel="shortcut icon" href="<?php bloginfo('template_url'); ?>/images/favico.ico" type="image/x-icon" />
-        <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/style.css">
-        <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/style.css">
+
+        <link rel="shortcut icon" href="<?= bloginfo('template_url'); ?>/images/favico.ico" type="image/x-icon">
+        <link rel="stylesheet" href="<?= bloginfo('template_url'); ?>/style.css">
+        <link rel="stylesheet" href="<?= bloginfo('template_url'); ?>/css/style.css">
+
+        <script src="<?= bloginfo('template_url'); ?>/js/vendor/modernizr.js"></script>
         
         <?php
 			wp_enqueue_script('jquery');
@@ -18,8 +19,8 @@
             wp_enqueue_script('script', get_template_directory_uri() . '/js/script.js', 'jquery', false);
 		?>
         <?php wp_head(); ?>
-        <?php if ( is_home() && !get_option('ss_disable') ) : ?>
-        <script type="text/javascript">
+        <?php if ( is_home() ) : ?>
+        <script>
             (function($) {
                 $(function() {
                     $('#slideshow').cycle({
@@ -29,11 +30,11 @@
                         prev:   '#larr'
                     });
                 })
-            })(jQuery)
+            })(jQuery);
         </script>
         <?php endif; ?>
 	</head>    
-	<body>
+	<body <?php body_class(); ?>>
     
         <div class="page-wrap">
         <div class="wrapper hfeed">
@@ -52,14 +53,8 @@
                     <a href="http://www.twitter.com/pgmisnomer" class="twitter" target="_blank"></a>
                     <a href="http://www.facebook.com/playgroundmisnomer" class="facebook" target="_blank"></a>            
                 </div>
-                
-                
-                <!-- ACTIVATE ONCE ADS ARE READY <a href="<?php // echo (get_option('header-ad-link')) ? get_option('header-ad-link') : '' ?>"><img class="header-ad" src="<?php // echo (get_option('header-ad')) ? get_option('header-ad') : '' ?>" width="728" height="90"/></a> -->
-                
+
                 <?php get_search_form(); ?>
-
-
-                
 
             </div>
 
