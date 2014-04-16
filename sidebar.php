fea1d820343b4b15684fd1e70629775e270e8142
Scott Donaldson <scott.p.donaldson@gmail.com>
2014-04-16 17:08:48 -0400

    sidebar

--- sidebar.php
+++ sidebar.php
@@ -1,4 +1,21 @@
 <div class="sidebar">
-    <a href="http://www.playgroundmisnomer.com/advertising"><img src="http://www.playgroundmisnomer.com/wp-content/themes/playgroundmisnomer/images/PM-advertising.png" alt="Playground Misnomer Advertising" /></a>
-           
+
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+
+	<!-- PM Sidebar 1 -->
+	<ins class="adsbygoogle"
+	     style="display:inline-block;width:300px;height:250px"
+	     data-ad-client="ca-pub-7985568994999759"
+	     data-ad-slot="8248788370"></ins>
+	<!-- PM Sidebar 2 -->
+
+	<ins class="adsbygoogle"
+	     style="display:inline-block;width:300px;height:250px"
+	     data-ad-client="ca-pub-7985568994999759"
+	     data-ad-slot="9725521572"></ins>
+	     
+	<script>
+	(adsbygoogle = window.adsbygoogle || []).push({});
+	</script>   
+
 </div>
