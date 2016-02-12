c6202012181ab3bc7889141688abe7a3bc1a0d33
Scottland Donaldson <scott.p.donaldson@gmail.com>
2016-02-12 10:27:57 -0500

    pm.scottland.cc changes

--- footer.php
+++ footer.php
@@ -5,30 +5,30 @@
             <!-- /Container -->
            
             <div id="sidebar">
-				<?php get_sidebar(); ?>
-			</div>
+                <?php get_sidebar(); ?>
+            </div>
             
             <div class="footer">
-            	<div class="footer-sidebar" <?php echo (get_option('border_color')) ? 'style="border-bottom: 26px solid '.get_option('border_color').';"' : '' ?>>
-            	<?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Footer') ) : ?>
-				<?php endif; ?>
-            	</div>
+                <div class="footer-sidebar" <?php echo (get_option('border_color')) ? 'style="border-bottom: 26px solid '.get_option('border_color').';"' : '' ?>>
+                <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Footer') ) : ?>
+                <?php endif; ?>
+                </div>
             
             </div>
 
             </div><!-- WRAPPER -->
             
             <div id="footer-info">
-            	<div class="copyright" style="float:left;">
-                	<p>&copy; <?php echo date('Y'); ?> <a href="<?php bloginfo('home'); ?>"><?php bloginfo('name'); ?></a>. All Rights Reserved.</p>
+                <div class="copyright" style="float:left;">
+                    <p>&copy; 2011-<?php echo date('Y'); ?> <a href="<?php bloginfo('home'); ?>"><?php bloginfo('name'); ?></a>. All Rights Reserved.</p>
                 </div>
                 
                 <div class="copyright" style="float:right;text-align:right;">
-                	<p><a href="http://www.playgroundmisnomer.com/staff">Staff</a> | <a href="http://www.playgroundmisnomer.com/advertising">Advertising</a></p>
+                    <p><a href="http://www.playgroundmisnomer.com/staff">Staff</a> | <a href="http://www.playgroundmisnomer.com/advertising">Advertising</a></p>
                 </div>
                 
 
-        	</div>
+            </div>
             
 
             </div><!-- .page-wrap -->
@@ -48,5 +48,5 @@
         </script>
         <?php } ?>
 
-	</body>
+    </body>
 </html>
